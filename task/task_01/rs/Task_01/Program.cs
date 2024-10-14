1088d2c462d93f92eb73e35d7f8a7303e26da4da
talinedali <aaaaaphp@yandex.ru>
2024-10-14 19:24:29 +0300

    Lab 1-2

---
+++ task/task_01/rs/Task_01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    { static void Main(string[] args)
+      {
+        Console.Write("Введите число: ");
+        double n = Convert.ToDouble(Console.ReadLine());
+            n = (Math.Pow(-1,n) * (1 - (n + 1) / (n + 1)));
+            Console.WriteLine(n);
+      }
+    }
+}
+
